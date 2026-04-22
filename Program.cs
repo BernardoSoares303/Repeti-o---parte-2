c061ebbb44ea0cd3dc531c31418d943800cb9ad8
BernardoSoares303 <bbnado34@gmail.com>
2026-04-22 19:02:00 -0300

    atividade 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int soma = 0;
+
+for(int i = 0; i <= 500; i++)
+{
+    if(i % 2 != 0 && i % 3 == 0)
+    {
+        soma += i;
+    }
+}
+
+Console.WriteLine($"A soma dos nnumeros impares do 1 ao 500 é: {soma}");
